a39c93e6e12963e1671adebde218148a0778e483
Bo Meier <bo.meier@gmail.com>
2014-05-22 22:06:59 +0200

    added Strings.lua

---
+++ Strings.lua
@@ -0,0 +1,16 @@
+local str = {}
+
+str["ITEM_QUALITY_TRASH"] = "Trash"
+str["ITEM_QUALITY_NORMAL"] = "Normal"
+str["ITEM_QUALITY_MAGIC"] = "Magic"
+str["ITEM_QUALITY_ARCANE"] = "Arcane"
+str["ITEM_QUALITY_ARTIFACT"] = "Artifact"
+str["ITEM_QUALITY_LEGENDARY"] = "Legendary"
+
+
+--filter options
+str["FILTER_OPTIONS_FILTER_BY"] = "Filter By"
+str["FILTER_OPTIONS_FILTER_NAME"] = "Filter Name"
+
+
+backpack.STRINGS = str
