cce0a7c7a91e53af1b964709e8006f30b4f4c9a0
Bo Meier <bo.meier@gmail.com>
2014-06-03 19:09:41 +0200

    quick fixed recursion

--- filter/FilterList.lua
+++ filter/FilterList.lua
@@ -9,23 +9,25 @@ function FilterList:New( )
 	return r
 end
 
+local calling = false
 function FilterList:Matches(slot, options)
 	if not options then return false end
 
 	if not options.filter then return false end
 	if not options.op then return false end
-
+	if calling == true then calling = false; return false end
+	
 	local matches = true
 	if options.op == ff.BOOLEAN_OPS.OR  then
 		matches = false
 	end
 	for _, filter in pairs(options.filter) do
-		if filter == self.name then
-			return false
-		end
+		
 		local data = BACKPACK.settings.filter[filter]
 		if not data then return false end
+		calling = true
 		local filterMatches = ff:GetFilter(data.type):Matches(slot, data.options)
+		calling = false
 		if options.op == ff.BOOLEAN_OPS.OR  then
 			if  filterMatches == true then
 				return true
